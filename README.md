a00d1c9f3e465b87724fa5794f208cfdf46aac73
DJ Soults <dsoults@hy-vee.com>
2018-01-31 15:58:17 -0600

    Initial implementation of script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # git_shortcuts
+
+A quick implementation of the shortcuts recommended at [https://git-scm.com/book/en/v1/Git-Basics-Tips-and-Tricks], including:
+- Auto-completion
+- Common aliases
+
+Note: This script is compatible with Mac and Linux systems only. For Windows systems, see the recommendations in the Git documentation.
+
+### Instructions:
+To add all commands listed in the Git documentation (linked above) run this command from a terminal:
+`curl -s URL | bash -s`
---
+++ git_tips.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+# Add Git Completion
+curl -o ~/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+source ~/git-completion.bash
+
+# Add Global Git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.visual '!gitk'
